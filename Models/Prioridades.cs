d54fbf931be6aad58ff17543cdcbc0d4114f2539
Braylin <braylinvasquez20@gmail.com>
2024-01-12 15:25:18 -0400

    Agregando la clase Contexto y Prioridades a sus respectivas carpetas

---
+++ Models/Prioridades.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RegPrioridades.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        private int PrioridadId {  get; set; }
+        [Required (ErrorMessage ="El campo es requerido")]
+        private string Descripción { get; set; }
+        [Required(ErrorMessage ="El campo es requerido")]
+        [Range(1,31, ErrorMessage ="Debe número estar entre 1 y 31")]
+        private int DíasCompromiso { get; set; }
+
+    }
+}
