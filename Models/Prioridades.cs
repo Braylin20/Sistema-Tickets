ddc2da2cd1b0d90517dfb5a56c8a2fc280a2fa1a
Braylin <braylinvasquez20@gmail.com>
2024-01-14 22:13:20 -0400

    Corrigiendo errores pequeños

--- Models/Prioridades.cs
+++ Models/Prioridades.cs
@@ -8,7 +8,6 @@ namespace RegPrioridades.Models
         [Required(ErrorMessage = "El campo es requerido")]
         public string Descripción { get; set; }
         [Required(ErrorMessage = "El campo es requerido")]
-        [Range(1, 31, ErrorMessage = "Debe número estar entre 1 y 31")]
         public int DíasCompromiso { get; set; }
     }
 }
