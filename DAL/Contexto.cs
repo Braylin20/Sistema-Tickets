05afaf4efaa1de9d50454ea9a85a2af17d1ac6da
Braylin <braylinvasquez20@gmail.com>
2024-01-29 11:52:36 -0400

    Agregando los services al contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -9,5 +9,6 @@ namespace RegPrioridades.DAL
         public DbSet<Prioridades> Prioridades {  get; set; }
         public DbSet<Clientes> Clientes { get; set; }
         public DbSet<Tickets> Tickets { get; set; }
+        public DbSet<Sistemas> Sistemas { get; set; }
     }
 }
